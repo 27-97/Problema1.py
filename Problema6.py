8d1dbbb8878332bfd0531066d61f12d074b2b3e7
27-97 <L24230239@minatitlan.tecnm.mx>
2024-11-10 00:10:27 -0600

    Create Problema6.py

---
+++ Problema6.py
@@ -0,0 +1,9 @@
+#Dados 3 números determinar el mayor e informar por pantalla el resultado
+
+num1 = float(input("Ingrese el primer número:"))
+num2 = float(input("Ingrese el segundo número:"))
+num3 = float(input("Ingrese el tercer número:"))
+
+nummayor = max(num1, num2, num3)
+  
+print(f"El número mayor es: {nummayor}")
