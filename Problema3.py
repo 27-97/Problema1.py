362e6e88744510b714292da8626eaf2336807ae0
27-97 <L24230239@minatitlan.tecnm.mx>
2024-11-09 23:53:28 -0600

    Update Problema3.py

--- Problema3.py
+++ Problema3.py
@@ -1,4 +1,5 @@
 #Ingresar la nota del examen de programación y si es mayor o igual a siete imprimir que aprobó, sino que debe recuperar.
+
 nota= int(input("ingrese nota"))
 if nota >= 70:
   print("aprobo")
