254d4d762dcb5bf7a28d18c1618511f6b98ac5a0
27-97 <L24230239@minatitlan.tecnm.mx>
2024-11-10 00:38:53 -0600

    Create Problema10.py

---
+++ Problema10.py
@@ -0,0 +1,11 @@
+#En un curso de computación, que consta de 24 alumnos, deberán armar un
+#algoritmo que informe por pantalla el apellido y nombre junto a la nota de examen de
+#cada alumno.
+
+for alumno in range(1, 25):
+    nombre = input(f"Ingrese el nombre del alumno {alumno}: ")
+    apellido = input(f"Ingrese el apellido del alumno {alumno}: ")
+    nota = float(input(f"Ingrese la nota del examen del alumno {alumno}: "))
+    print(f"Alumno: {apellido}, {nombre} - Nota: {nota}")
+
+
