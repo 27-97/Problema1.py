1973f37ba33b75acb04c83cf0d933cc29542a5c5
27-97 <L24230239@minatitlan.tecnm.mx>
2024-11-10 01:06:37 -0600

    Create Problema13.py

---
+++ Problema13.py
@@ -0,0 +1,16 @@
+#Ingresar 10 valores por teclado y obtener la sumatoria de los mismos. Imprimir los
+#resultados.
+
+valor1= int(input("ingrese numero")) 
+valor2= int(input("ingrese numero")) 
+valor3= int(input("ingrese numero")) 
+valor4= int(input("ingrese numero")) 
+valor5= int(input("ingrese numero")) 
+valor6= int(input("ingrese numero")) 
+valor7= int(input("ingrese numero")) 
+valor8= int(input("ingrese numero")) 
+valor9= int(input("ingrese numero")) 
+valor10= int(input("ingrese numero")) 
+
+suma= ("la suma es igual a", valor1 + valor2 +valor3 + valor4 + valor5 + valor6 + valor7 + valor8 + valor9 + valor10)
+print(suma)
