7efa60c1ea46e9cf4929ec06dab7b84033f5b081
27-97 <L24230239@minatitlan.tecnm.mx>
2024-11-10 01:12:06 -0600

    Create Problema14.py

---
+++ Problema14.py
@@ -0,0 +1,13 @@
+#Ingresar 10 valores numéricos por teclado y calcular la suma, el promedio e
+#imprimir la suma, el promedio agregando una leyenda en cada caso.
+
+suma = 0
+
+for num in range(10):
+    valor = float(input("Ingrese un valor: "))
+    suma += valor
+
+promedio = suma / 10
+
+print(f"La suma es: {suma}")
+print(f"El promedio es: {promedio}")
