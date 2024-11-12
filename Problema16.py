5dcafca1b6a7bdde079456328fcde12c83c82d32
CUELLAR <L24230239@minatitlan.tecnm.mx>
2024-11-11 21:35:02 -0600

    Update Problema16.py

--- Problema16.py
+++ Problema16.py
@@ -2,17 +2,17 @@
 #comprendidos entre 5 y 2500 ambos inclusive, imprimir el resultado.
 
 suma = 0
-contador = 0
+conta = 0
 
 for y in range(10):
     valor = float(input("Ingrese un valor: "))
 
     if 5 <= valor <= 2500:
         suma += valor
-        contador += 1
+        conta += 1
 
-if contador > 0:
-    promedio = suma / contador
+if conta > 0:
+    promedio = suma / conta
     print(f"El promedio es: {promedio}")
 else:
     print("No se ingresaron valores en el rango especificado.")
