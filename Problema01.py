f1892627c525e2f718d4d7d008a6cc64dcd7e705
CUELLAR <L24230239@minatitlan.tecnm.mx>
2024-11-12 19:28:41 -0600

    Update Problema01.py

--- Problema01.py
+++ Problema01.py
@@ -1,6 +1,6 @@
 #Hacer un programa que sume dos números y muestre el resultado
 
-a= int(input("ingrese número))
-b= int(input("ingrese número))
-Suma= a + b
-print("Resultado", Suma)  
+z = int(input("ingrese número"))
+w = int(input("ingrese número"))
+Suma= z + w
+print("Resultado", Suma)
